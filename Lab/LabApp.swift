b05b13b375d62773c0fccaf15b52a738718600d0
Sofiia Beliak <101469384@georgebrown.ca>
2026-02-25 20:55:20 -0500

    Initial Commit

---
+++ Lab/LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabApp.swift
+//  Lab
+//
+//  Created by Maks Voshchenko on 2026-02-25.
+//
+
+import SwiftUI
+
+@main
+struct LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
