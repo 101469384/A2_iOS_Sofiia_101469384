19d98b425a8e1c7da81194919ff869059f0b5731
Sofiia Beliak <101469384@georgebrown.ca>
2026-02-25 21:12:49 -0500

    Added Lab7 Multi-Screen Navigation App

---
+++ Lab7/Lab7/Lab7App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab7App.swift
+//  Lab7
+//
+//  Created by Maks Voshchenko on 2026-02-25.
+//
+
+import SwiftUI
+
+@main
+struct Lab7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
